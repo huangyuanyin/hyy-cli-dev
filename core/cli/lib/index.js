b2f1915d64b30c05b2a00810f01394d60ce8821f
黄原寅 <3443718488@qq.com>
2024-01-27 17:53:04 +0800

    add log

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -2,6 +2,21 @@
 
 module.exports = core
 
+// require：.js/ .json/ .node/
+// .js -> modules.exports/exports
+// .json -> JSON.parse
+// .node -> process.dlopen
+// any -> .js
+const pkg = require('../package.json')
+const log = require('@hyy-cli-dev/log')
+
 function core() {
-  console.log(`exec core`)
+  checkPkgVersion()
+}
+
+function checkPkgVersion() {
+  // console.log(pkg.version)
+  // log.success('test', 'success...')
+  // log.verbose('debug', 'debug...')
+  log.notice('cli', pkg.version)
 }
